d6fc76756300fef9110243bda5ad8e75c8c32385
sk7z-xyz <sk7zgo@gmail.com>
2022-07-31 22:22:49 +0900

    PowerShellの実行ポリシーの変更

--- START_VRC_AffinityChange_PS.bat
+++ START_VRC_AffinityChange_PS.bat
@@ -3,7 +3,6 @@ REM PowerShellのウィンドウを表示･非表示にしたい場合は､下
 REM 非表示の場合､プロセスを特定して終了することが難しいため､動作確認後に非表示にしてください｡
 
 REM PowerShellのウィンドウを表示する場合
-start powershell -File VRC_AffinityChange_PS.ps1
+start powershell -ExecutionPolicy Unrestricted -File VRC_AffinityChange_PS.ps1
 REM PowerShellのウィンドウを表示しない場合
-REM start powershell -File VRC_AffinityChange_PS.ps1 -WindowStyle Hidden
-
+REM start powershell-ExecutionPolicy Unrestricted -File VRC_AffinityChange_PS.ps1 -WindowStyle Hidden
